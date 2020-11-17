a0e457869818b87f97f7651731e195dc8062f618
infoprogrammr <68546554+infoprogrammr@users.noreply.github.com>
2020-11-17 21:55:22 +0530

    Create Challenge.java

---
+++ Challenge.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.Scanner;
+public class numtostring
+{
+	
+	public static String ones[]={"one","two","three","four","five","six"," seven", "eight","nine","ten","eleven","twelve","thirteen","forteen","fifteen","sixteen","seventeen","eighteen","ninteen"};      
+        public static String tens[]={"twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninty"};       
+public static void main(String a[]) throws Exception
+	{
+        Scanner sc=new Scanner(System.in);
+        int num,rem,t;   
+ 
+      
+         System.out.println("Enter the number:");
+          num=sc.nextInt();
+         System.out.println("Entered number is:");
+      /*write down your logic here*/
+       
+	  
+        }//main 
+
+
+	 
+}  
+  
+  
+
+
+
